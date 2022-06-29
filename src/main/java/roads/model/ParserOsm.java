1bc870f02d568a64bccf98015009b0894a44fa3d
RamilRakaev <ramrg546@gmail.com>
2022-06-29 16:08:11 +0300

    Проведена фильтрация по highway (исправил ошибки)

--- src/main/java/roads/model/ParserOsm.java
+++ src/main/java/roads/model/ParserOsm.java
@@ -5,46 +5,43 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
-public class Parsing_Osm {
+public class ParserOsm {
 
     Set<String> roadType = Stream.of("motorway", "trunk", "primary", "secondary", "tertiary", "unclassified", "residential", "motorway_link", "trunk_link", "primary_link", "secondary_link", "tertiary_link", "living_street", "service").collect(Collectors.toSet());
 
-    public Object Osm(Document doc) {
+    public Object parseOsm(Document doc) {
         Node osmNode = doc.getFirstChild();
         NodeList osmChilds = osmNode.getChildNodes();
         Node wayNode = null;
 
-        for(int i = 0; i < osmChilds.getLength(); i++) {
-            if(osmChilds.item(i).getNodeType() != Node.ELEMENT_NODE) {
+        for (int i = 0; i < osmChilds.getLength(); i++) {
+            if (osmChilds.item(i).getNodeType() != Node.ELEMENT_NODE) {
                 continue;
             }
 
-            if(osmChilds.item(i).getNodeName().equals("way")) {
+            if (osmChilds.item(i).getNodeName().equals("way")) {
                 wayNode = osmChilds.item(i);
                 NodeList wayChildNodes = wayNode.getChildNodes();
 
-                for(int j = 0; j < wayChildNodes.getLength(); j++) {
-                    if(wayChildNodes.item(i).getNodeType() != Node.ELEMENT_NODE) {
+                for (int j = 0; j < wayChildNodes.getLength(); j++) {
+
+                    Node tagChilds = wayChildNodes.item(j);
+                    if (tagChilds.getNodeType() != Node.ELEMENT_NODE) {
                         continue;
                     }
-                    Node tagChilds = wayChildNodes.item(i);
-                    NamedNodeMap atributes = tagChilds.getAttributes();
-                    String v = atributes.getNamedItem("v").getNodeValue();
-
-                    if(tagChilds.getNodeName().equals("tag")) {
-                        if(atributes.getNamedItem("k").getNodeValue().equals("highway")) {
+                    if (tagChilds.getNodeName().equals("tag")) {
+                        NamedNodeMap atributes = tagChilds.getAttributes();
+                        String v = atributes.getNamedItem("v").getNodeValue();
+                        if (atributes.getNamedItem("k").getNodeValue().equals("highway")) {
                             boolean right_road = roadType.add(v);
-                            if(!right_road) {
-                                return osmNode;
-                            }
-                            else {
+                            if (!right_road) {
+                                System.out.println(wayNode.getNodeName());
+                            } else {
                                 roadType.remove(v);
                             }
                         }
